425ac4b84ca58b9937de9a129f03a51deac59d28
Vlad <funnytrofi@gmail.com>
2022-07-19 15:30:15 +0600

    under 022 course Max

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,16 @@
-// ignore_for_file: unused_import
+// ignore_for_file: unused_import, avoid_print
 
 import 'package:flutter/material.dart';
+
+class Person {
+  var name = 'Vlad';
+  var age = 33;
+  myPsichologyAce() {
+    return age + 10;
+  }
+}
+
+void main() {
+  var p1 = Person();
+  print(p1.myPsichologyAce());
+}
